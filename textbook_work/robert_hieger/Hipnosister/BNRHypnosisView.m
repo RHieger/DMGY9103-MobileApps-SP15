e6af62bdfccd20869d65a56383145d7496093475
Robert Hieger <rhieger@nyc.rr.com>
2015-02-11 22:44:58 -0500

    Chapter 4: Hipnosister--Render Circle In Subview (Does Not Match Book)

--- textbook_work/robert_hieger/Hipnosister/BNRHypnosisView.m
+++ textbook_work/robert_hieger/Hipnosister/BNRHypnosisView.m
@@ -10,12 +10,48 @@
 
 @implementation BNRHypnosisView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
+// Override default CGRect Object.
+
+- (void) drawRect: (CGRect) rect {
+    
+    // Establish the bounds of the window.
+    
+    CGRect bounds = self.bounds;
+    
+    // Figure out the center of the bounds rectangle.
+    
+    CGPoint center;     // This variable stores the center point.
+    
+    // Determine centered X-coordinate.
+    
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    
+    // Determine centered Y-coordinate.
+    
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    // The circle will be the largest that will fit in the view
+    // (portrait or landscape).
+    
+    float radius = MIN(bounds.size.width, bounds.size.height);
+    
+    // Instantiate UIBezierPath Object in order to draw circle.
+    
+    UIBezierPath *path = [ [UIBezierPath alloc] init ];
+    
+    // Add an arc to the path at center, with radius of radius,
+    // with 0 to 2 * PI radians (a circle).
+    
+    [path addArcWithCenter: center
+                    radius: radius
+                startAngle: 0.0
+                  endAngle: M_PI * 2.0
+                 clockwise: YES];
+    
+    // Draw the circumference of the circle.
+    
+    [path stroke];
+    
+}   // end - (void) drawRect: (CGRect) rect
 
 @end
